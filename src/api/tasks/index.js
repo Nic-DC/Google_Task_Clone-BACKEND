af49ed7aecb1342fa4e8ea2f5ae9bea1caadde03
Nic <nic.dan.costea@gmail.com>
2023-01-03 11:12:14 +0100

    CRUD tasks - done

--- src/api/tasks/index.js
+++ src/api/tasks/index.js
@@ -51,18 +51,64 @@ tasksRouter.get("/", async (req, res, next) => {
 });
 
 // 3. GET: http://localhost:3005/tasks/:taskId
-tasksRouter.get("/:taskId", (req, res) => {
-  res.send({ message: "This is the GET single task route" });
+tasksRouter.get("/:taskId", async (req, res, next) => {
+  try {
+    const taskId = req.params.taskId;
+
+    const tasksList = await getTasks();
+
+    const task = tasksList.find((task) => task._id === taskId);
+
+    if (task) {
+      res.send(task);
+    } else {
+      next(NotFound(`The task with id: ${taskId} is not in your archive`));
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // 4. PUT: http://localhost:3005/tasks/:taskId
-tasksRouter.put("/:taskId", (req, res) => {
-  res.send({ message: "This is the PUT single task route" });
+tasksRouter.put("/:taskId", async (req, res, next) => {
+  try {
+    const { taskId } = req.params;
+
+    const tasksList = await getTasks();
+
+    const index = tasksList.findIndex((task) => task._id === taskId);
+
+    if (index !== -1) {
+      const oldTask = tasksList[index];
+      const updatedTask = { ...oldTask, ...req.body, updatedAt: new Date() };
+      tasksList[index] = updatedTask;
+
+      console.log("The updated task looks like this: ", updatedTask);
+
+      res.send(updatedTask);
+    } else {
+      next(NotFound(`The task with id: ${taskId} is not in your archive`));
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 // 5. DELETE: http://localhost:3005/tasks/:taskId
-tasksRouter.delete("/:taskId", (req, res) => {
-  res.send({ message: "This is the DELETE single task route" });
+tasksRouter.delete("/:taskId", async (req, res, next) => {
+  try {
+    const tasksList = await getTasks();
+
+    const remainingTasks = tasksList.filter((task) => task._id !== req.params.taskId);
+
+    if (tasksList.length !== remainingTasks.length) {
+      res.send({ message: `Task with id: ${req.params.taskId} deleted successfully` });
+    } else {
+      next(NotFound(`Task with id: ${req.params.taskId} is not in your archive`));
+    }
+  } catch (error) {
+    next(error);
+  }
 });
 
 export default tasksRouter;
